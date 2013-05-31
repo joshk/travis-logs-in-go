5a812aa3d943dfea5fc27aa52d2a4e6785cd5642
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-05-31 14:09:39 +0200

    using flags to determine which type of process should be started

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-//"strings"
+    //"strings"
+    "log"
+    "flag"
 )
 
+var process = flag.String("process", "streaming", "The process to start")
+
 func main() {
-    ProcessLogParts()
+    flag.Parse()
+    switch *process {
+    case "streaming":
+        log.Println("Starting Log Stream Processing")
+        ProcessLogParts()
+    case "aggregate":
+        log.Println("Starting Log Aggregation")
+        log.Println("WE NEED AGGREGATION")
+    default: 
+        panic("Invalid process option selected")
+    }
 }

f5238fd77bc153ce604ea330633982d1c69b5b01
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-09-03 10:32:06 +0200

    update the histogram output to match the timer

--- metrics.go
+++ metrics.go
@@ -70,7 +70,7 @@ func logMetrics(m *Metrics, l *log.Logger) {
             l.Printf("metriks: time=%d name=%s type=healthcheck error=%v\n", now, name, m.Error())
         case metrics.Histogram:
             ps := m.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
-            l.Printf("metriks: time=%d name=%s type=histogram count=%d min=%d max=%d mean=%f stddev=%f median=%f 95th_percentile=%f 99th_percentile=%f\n", now, name, m.Count(), m.Min(), m.Max(), m.Mean(), m.StdDev(), ps[0], ps[2], ps[3])
+            l.Printf("metriks: time=%d name=%s type=histogram count=%d min=%d max=%d mean=%f stddev=%f median=%f 95th_percentile=%f 99th_percentile=%f\n", now, name, m.Count(), int64NanoToSeconds(m.Min()), int64NanoToSeconds(m.Max()), float64NanoToSeconds(m.Mean()), float64NanoToSeconds(m.StdDev()), float64NanoToSeconds(ps[0]), float64NanoToSeconds(ps[2]), float64NanoToSeconds(ps[3]))
         case metrics.Meter:
             l.Printf("metriks: time=%d name=%s type=meter count=%d one_minute_rate=%f five_minute_rate=%f fifteen_minute_rate=%f mean_rate=%f\n", now, name, m.Count(), m.Rate1(), m.Rate5(), m.Rate15(), m.RateMean())
         case metrics.Timer:

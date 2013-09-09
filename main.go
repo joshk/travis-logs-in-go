61b54c8126dbfdb9376fd87a61c550b41244b9db
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-09-09 03:30:08 +0200

    do some pprofing!

--- main.go
+++ main.go
@@ -2,16 +2,39 @@ package main
 
 import (
     "flag"
+    "github.com/davecheney/profile"
     "log"
+    "os"
+    "os/signal"
+    "syscall"
 )
 
 var process = flag.String("process", "streaming", "The process to start")
+
+func profileHandler() {
+    p := profile.Start(&profile.Config{
+        CPUProfile:   true,
+        MemProfile:   true,
+        BlockProfile: true,
+    })
+
+    sig := make(chan os.Signal, 1)
+    signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+
+    <-sig
+
+    p.Stop()
+
+    os.Exit(0)
+}
 
 func init() {
     log.SetFlags(0)
 }
 
 func main() {
+    go profileHandler()
+
     flag.Parse()
     switch *process {
     case "streaming":
